349911da5e2dcdd373a789bb0834b02a6a67f23f
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-07-07 00:52:01 +0530

    adding the randomness to the dice rolling

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
+Random randomGenerator = Random();
+
+// add the statfulwidget here
 class DiceRoller extends StatefulWidget {
   DiceRoller({super.key});
 
   @override
+
+  // this needs a state of this object
+  // therefore we should create another class for this
   State<DiceRoller> createState() {
     return _DiceRollerState();
   }
 }
 
+// this is a private class
+// behaves almost like a stateless widget
+// but maintains the state
 class _DiceRollerState extends State<DiceRoller> {
-  String activeDiceImage = 'assets/images/dice-2.png';
+  int currentImageNumber = 2;
 
   void rollDice() {
+    // change the image and re-render it
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentImageNumber = randomGenerator.nextInt(6) + 1;
     });
   }
 
+  // return the widget as a stateless widget
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentImageNumber.png',
           width: 200,
         ),
         const SizedBox(
