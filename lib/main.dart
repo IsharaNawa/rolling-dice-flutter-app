93b8602fffed98bdccd483fe72ba535549ec918d
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-07-06 23:50:47 +0530

    added the custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,38 @@ void main() {
 
   // background gradient can be added using decoration parameter in container
 
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 49, 11, 125),
-              Color.fromARGB(255, 54, 19, 152)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+  runApp(const MaterialApp(
+    home: Scaffold(body: GradientContainer()),
+  ));
+}
+
+// created a new custom widget
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 49, 11, 125),
+            Color.fromARGB(255, 54, 19, 152)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: const Center(
-          child: Text(
-            "Hello World!",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-            ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
