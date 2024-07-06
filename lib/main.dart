425a7aabe0bf2f948d72d64eb78e418b9c2b0c3b
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-07-06 23:17:25 +0530

    added the initial screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  // MaterialApp widget should be inside the runApp function
+
+  // home (named) argument should be defined for MaterialApp widget
+
+  // since all the things in the MaterialApp, we can add const keyword
+  // to improve runtime performance
+  runApp(const MaterialApp(
+    home: Text("Hello World!"),
+  ));
 }
